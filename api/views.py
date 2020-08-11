9cffb48f385f88c784ac23b5d7ff30f431b2df42
Aria Moradi <aria.moradi007@gmail.com>
2020-08-11 20:25:02 +0430

    return last one on GET request

--- api/views.py
+++ api/views.py
@@ -1,10 +1,23 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import mixins, generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from .models import DateTime
 from .serializers import DateTimeSerializer
 
 
-class DateTimeView(ListCreateAPIView):
+class DateTimeView(mixins.CreateModelMixin,
+                   generics.GenericAPIView):
     queryset = DateTime.objects.all()
     serializer_class = DateTimeSerializer
 
+    def last(self, request, *args, **kwargs):
+        instance = self.queryset.last()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def get(self, request, *args, **kwargs):
+        return self.last(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
